db5e0d9a8a0d2b2a8677229122fff2c30c9d3cff
Yunus Emre <164664008+unizeii@users.noreply.github.com>
2024-04-17 18:42:36 +0300

    Update DayOfYear.cpp

--- DayOfYear.cpp
+++ DayOfYear.cpp
@@ -6,14 +6,17 @@ using namespace std;
 class DayOfYear {
 private:
     int day;
-    static const int daysPerMonth[];
-    static const string nameOfMonths[];
+    static const int daysPerMonth[]; // Static array to hold the number of days in each month
+    static const string nameOfMonths[]; // Static array to hold the names of each month
 
 public:
+    // Constructor to initialize the day
     DayOfYear(int d) : day(d) {}
 
+    // Function to print the corresponding date
     void print() {
         int month = 0;
+        // Subtracting the days of each month from the total until we find the corresponding month
         while (day > daysPerMonth[month]) {
             day -= daysPerMonth
     [month];
@@ -23,6 +26,7 @@ public:
     }
 };
 
+// Definition of the static arrays holding days per month and month names
 const int DayOfYear::daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 const string DayOfYear::nameOfMonths[12] = {
@@ -36,6 +40,7 @@ int main() {
     cout << "Enter the day of the year: ";
     cin >> inputDay;
 
+    // Creating an instance of DayOfYear class with the user input
     DayOfYear day(inputDay);
     cout << "The corresponding date is: ";
     day.print();
